5269e4b7c99da8511b1526370bf155834fbc5972
unknown <836697677@qq.com>
2021-04-10 16:31:47 +0800

    init repo

---
+++ src/test/java/ball/ingram/com/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ball.ingram.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
